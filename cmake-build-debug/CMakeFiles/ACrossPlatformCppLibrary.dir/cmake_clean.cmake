4b929fa5063b2c884820b1bece5de95771f83516
Ciaran Welsh <cwelsh2@uw.edu>
2020-02-23 11:06:09 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/ACrossPlatformCppLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ACrossPlatformCppLibrary.exe"
+  "ACrossPlatformCppLibrary.exe.manifest"
+  "ACrossPlatformCppLibrary.lib"
+  "ACrossPlatformCppLibrary.pdb"
+  "CMakeFiles/ACrossPlatformCppLibrary.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ACrossPlatformCppLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
